2182823fd116856a055212751465b8fd8573f65a
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-16 22:48:54 -0400

    Instalação e setup do módulo de animações do angular + web-animations-js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -16,9 +17,10 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     BannerComponent,
     LoginComponent,
     CadastroComponent
- ],
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
