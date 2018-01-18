87b9309ea9504e64187c5e4a3bdad6fe883eedcf
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-17 21:27:28 -0400

    Conectando o Firebase SDK do projeto com o Back-end do Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+
+  ngOnInit(): void {
+    // Initialize Firebase
+    let config = {
+      apiKey: "AIzaSyDP6PZ3vk3RUYiPAXDYiXci15zs9xuuEVE",
+      authDomain: "jta-instagran-clone-69163.firebaseapp.com",
+      databaseURL: "https://jta-instagran-clone-69163.firebaseio.com",
+      projectId: "jta-instagran-clone-69163",
+      storageBucket: "jta-instagran-clone-69163.appspot.com",
+      messagingSenderId: "200917085093"
+    };
+    firebase.initializeApp(config);
+  }
+
+
 }
