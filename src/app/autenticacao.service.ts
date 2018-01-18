a2fcc97c5536399170302eddd24fab9126b6a13e
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-17 21:34:22 -0400

    Cadastrando Usuário no Firebase

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,8 +1,16 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from 'firebase';
+
 
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario):void {
-            console.log('chegamos', usuario);
+        firebase.auth()
+        .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+        .then((resposta: any) => {
+            console.log(resposta);
+        }).catch((error: any) => {
+            console.log(error);
+        })
     }
 }
