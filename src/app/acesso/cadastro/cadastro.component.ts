3ff75e71f84d47fb8b684e1a9b129ceb00db4bbd
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-17 21:08:58 -0400

    Criando o model Usuario

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { Usuario } from '../usuario.model';
 
 @Component({
   selector: 'app-cadastro',
@@ -26,5 +28,17 @@ export class CadastroComponent implements OnInit {
 
   public exibirPainelLogin(): void {
     this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario(): void {
+
+    let usuario = new Usuario(
+      this.formulario.value.email,
+      this.formulario.value.nome_completo,
+      this.formulario.value.nome_usuario,
+      this.formulario.value.senha
+    );
+
+    console.log(usuario);
   }
 }
