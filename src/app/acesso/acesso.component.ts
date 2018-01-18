1c871991ea7084101c097f86248242e4bb7c9313
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-17 20:46:20 -0400

    Aplicando animação com keyframes no painel login e cadastro

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
@@ -26,7 +26,21 @@ import { Component, OnInit, trigger, state, style, transition, animate } from '@
           opacity: 0,
           transform: 'translate(50px, 0)'
         }),
-        animate('500ms 0s ease-in-out')
+        animate('1500ms 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1 , transform:'translateX(0)'}),
+          style({ offset: 0.86, opacity: 1 , transform:'translateX(0)'}),
+          style({ offset: 0.88, opacity: 1 , transform:'translateY(-10px)'}),
+          style({ offset: 0.90, opacity: 1 , transform:'translateY(10px)'}),
+          style({ offset: 0.92, opacity: 1 , transform:'translateY(-10px)'}),
+          style({ offset: 0.94, opacity: 1 , transform:'translateY(10px)'}),
+          style({ offset: 0.96, opacity: 1 , transform:'translateY(-10px)'}),
+          style({ offset: 0.98, opacity: 1 , transform:'translateY(-10px)'}),
+
+          style({ offset: 1, opacity: 1 , transform:'translateY(0)'})
+       
+       
+     
+        ]))
       ])
     ])
   ]
