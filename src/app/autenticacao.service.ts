195fae1741b9dd4089cf95efb7e4b23a44168567
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-20 09:46:25 -0400

    Persistindo o token_id no localStorage

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,6 +1,6 @@
 import { Usuario } from "./acesso/usuario.model";
 import * as firebase from 'firebase';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { Injectable } from "@angular/core";
 
 
@@ -14,32 +14,38 @@ export class Autenticacao {
     }
 
     public cadastrarUsuario(usuario: Usuario): Promise<any> {
-       return  firebase.auth()
-        .createUserWithEmailAndPassword(usuario.email, usuario.senha)
-        .then((resposta: any) => {
-            delete usuario.senha;
+        return firebase.auth()
+            .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                delete usuario.senha;
 
-           firebase.database()
-                   .ref(`usuario_detalhe/${btoa(usuario.email)}`)
-                   .set(usuario);
-        }).catch((error: any) => {
-            console.log(error);
-        })
+                firebase.database()
+                    .ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    .set(usuario);
+            }).catch((error: any) => {
+                console.log(error);
+            })
     }
 
 
     public autenticar(email: string, senha: string): void {
-        firebase.auth().signInWithEmailAndPassword(email,senha)
-        .then((resposta: any) => {
-            firebase.auth().currentUser.getIdToken().then( (idToken: string) => {
-                this.token_id = idToken;
-                this.router.navigate(['/home'])
-            }) 
-        })
-        .catch((error: Error) => console.log(error))
+        firebase.auth().signInWithEmailAndPassword(email, senha)
+            .then((resposta: any) => {
+                firebase.auth().currentUser.getIdToken().then((idToken: string) => {
+                    this.token_id = idToken;
+                    localStorage.setItem('idToken', idToken);
+                    this.router.navigate(['/home'])
+                })
+            })
+            .catch((error: Error) => console.log(error))
     }
 
     public autenticado(): boolean {
+        let tokenInLocal = localStorage.getItem('idToken');
+
+        if (this.token_id === undefined && tokenInLocal != null) {
+            this.token_id = tokenInLocal;
+        }
         return this.token_id !== undefined;
     }
 }
